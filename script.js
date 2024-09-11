b033a6a83defeb2ff5b98a6b06702030c6ef75a0
saeed rahimi <saeedteachcodee@email.com>
2024-09-11 14:01:25 +0330

    finish

--- script.js
+++ script.js
@@ -25,3 +25,62 @@ function OddEvencheker(n) {
 console.log(OddEvencheker(num1));
 console.log(OddEvencheker(num));
 ////////////////////////////////////////////////
+
+
+const age = 14;
+
+if (age >= 18) {
+    console.log("you are an aukt");
+
+} else if (age > 13 && age <= 17) {
+    console.log(" you are tenager");
+
+} else {
+    console.log("you are child");
+} //////////////////////////////////////
+let grade;
+let score = 50
+
+if (score >= 90 && score <= 100) {
+    grade = "A+";
+} else
+if (score >= 80 && score <= 89) {
+    grade = "A";
+} else if (score >= 70 && score <= 79) {
+    grade = "B+";
+} else if (score >= 60 && score <= 69) {
+    grade = "B";
+} else if (score >= 50 && score <= 59) {
+    grade = "C";
+} else if (score < 50) {
+    grade = "D";
+} else {
+    grade = "Invalid score";
+}
+
+console.log(`The grade for a score of ${score} is: ${grade}`);
+////////////////////////////////
+
+let weight = 75;
+let height = 1.8;
+
+
+let bmi = weight / (height * height);
+
+
+let range;
+
+
+if (bmi < 18.5) {
+    range = "Underweight";
+} else if (bmi >= 18.5 && bmi < 25) {
+    range = "Normal weight";
+} else if (bmi >= 25 && bmi < 30) {
+    range = "Overweight";
+} else {
+    range = "Obese";
+}
+
+
+
+console.log(` Your Health is : ${range}`);
